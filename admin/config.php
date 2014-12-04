a90ab5d7308b476be837d0c2390c9f8748fcba46
Serj <serega2000_07@mail.ru>
2014-12-04 21:44:27 +0600

    First commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/magazin.kz/admin/');
+define('HTTP_CATALOG', 'http://localhost/magazin.kz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/magazin.kz/admin/');
+define('HTTPS_CATALOG', 'http://localhost/magazin.kz/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\magazin.kz/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\magazin.kz/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\magazin.kz/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\magazin.kz/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\magazin.kz/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\magazin.kz/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\magazin.kz/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\magazin.kz/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\magazin.kz/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\magazin.kz/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\magazin.kz/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
